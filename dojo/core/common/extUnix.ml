2c74806595f812bb4a5769afd2af6e80d7c1c14a
Regis-Gianas <yrg@pps.univ-paris-diderot.fr>
2014-07-17 10:10:10 +0200

    * Comment.

--- dojo/core/common/extUnix.ml
+++ dojo/core/common/extUnix.ml
@@ -164,6 +164,10 @@ let pdflatex content outputfile = Filename.(
     )
   in
   !>> pdflatex
+  (* FIXME: If the compilation failed, we want to create a diagnostic and
+  (* FIXME: send a bug report to us. Indeed, we shall make sure that our
+  (* FIXME: generated LaTeX is always well-formed. By the way, user-written
+  (* FIXME: LaTeX should be processed separately. *)
   >>> cp (base ^ ".pdf") outputfile
   >>> cleanup
 )
