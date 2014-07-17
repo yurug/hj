bdbea24e8dbc3f71b116da59ea9b0a30293389c9
Regis-Gianas <yrg@pps.univ-paris-diderot.fr>
2014-07-17 10:57:57 +0200

    * InMemory.ml.

---
+++ dojo/core/reactive/inMemory.ml
@@ -0,0 +1,128 @@
+(* -*- tuareg -*- *)
+
+(** In-memory representation of an entity state. *)
+
+open Identifier
+open Property
+
+type dependency_kind = string
+
+type dependency = identifier * (dependency_kind * identifier list)
+
+(* FIXME: Optimize that naive representation if needed. *)
+type dependencies =
+    (string * ((identifier list * identifier) list)) list
+    deriving (Json)
+
+let empty_dependencies =
+  []
+
+let dependency_image (x : dependencies) =
+  List.flatten (List.map (fun (l, rel) ->
+    List.map (fun (xs, y) -> (y, (l, xs))) rel)
+  x)
+
+let push (deps : dependencies) ((y, (l, xs)) : dependency) =
+  let rel = try List.assoc l deps with Not_found -> [] in
+  let deps = List.remove_assoc l deps in
+  let rel = if List.mem (xs, y) rel then rel else (xs, y) :: rel in
+  (l, rel) :: deps
+
+let of_list x = x
+
+let to_list x = x
+
+let dependency d k xs =
+  try
+    Some (List.assoc xs (List.assoc k d))
+  with Not_found -> None
+
+let all_dependencies d k =
+  try
+    List.assoc k d
+  with Not_found -> []
+
+type 'a meta = {
+  identifier      : identifier;
+  dependencies    : dependencies;
+  properties      : properties;
+  sources         : Resource.name list;
+  content         : 'a;
+  tick            : float;
+} deriving (Json)
+
+let now e = { e with tick = Unix.gettimeofday () }
+
+let make identifier dependencies properties sources content =
+  now { identifier; dependencies; content; properties; sources; tick = 0. }
+
+let sources e = e.sources
+
+let identifier e = e.identifier
+
+let dependencies e = e.dependencies
+
+let properties e = e.properties
+
+let content e = e.content
+
+let timestamp e = e.tick
+
+let update_content e c = now { e with content = c }
+
+let update_properties e s = now { e with properties = s }
+
+let update_dependencies e d = now { e with dependencies = d }
+
+let update_sources e s =
+  let sources = s @ List.(filter (fun x -> not (mem x s)) e.sources) in
+  now { e with sources }
+
+type 'a state_change =
+  | UpdateDependencies of dependencies
+  | UpdateResources    of Resource.name list
+  | UpdateProperties   of properties
+  | UpdateContent      of 'a
+  | UpdateSequence     of 'a state_change * 'a state_change
+  | NoUpdate
+(* FIXME: If state changes happen to be too large, we will move
+   to a more intentional description, using diffs. *)
+
+let rec state_changes = function
+  | [] ->
+    NoUpdate
+  | NoUpdate :: cs ->
+    state_changes cs
+  | UpdateSequence (NoUpdate, c) :: cs ->
+    state_changes (c :: cs)
+  | UpdateSequence (c1, c2) :: cs ->
+    UpdateSequence (c1, state_changes (c2 :: cs))
+  | c :: cs ->
+    UpdateSequence (c, state_changes cs)
+
+let rec update e = function
+  | UpdateDependencies d -> update_dependencies e d
+  | UpdateResources s -> update_sources e s
+  | UpdateProperties s -> update_properties e s
+  | UpdateContent c -> update_content e c
+  | UpdateSequence (c1, c2) -> update (update e c1) c2
+  | NoUpdate -> e
+
+let rec string_of_state_change string_of_replacement = function
+  | UpdateDependencies _ ->
+    "Update dependencies"
+  | UpdateResources s ->
+    Printf.sprintf "Update sources (%s)" (String.concat " " s)
+  | UpdateProperties s ->
+    "Update properties"
+  | UpdateContent young ->
+    "Update content"
+  | UpdateSequence (c1, NoUpdate) | UpdateSequence (NoUpdate, c1)->
+    string_of_state_change string_of_replacement c1
+  | UpdateSequence (c1, c2) ->
+    string_of_state_change string_of_replacement c1 ^ "; "
+    ^ string_of_state_change string_of_replacement c2
+  | NoUpdate ->
+    "No update"
+
+let map f m = { m with content = f m.content }
---
+++ dojo/core/reactive/inMemory.mli
@@ -0,0 +1,117 @@
+(* -*- tuareg -*- *)
+
+(** In-memory (purely functional) representation of entities state.
+
+    The state of an entity contains:
+    - specific data ;
+    - a set of properties (see {!Property}) ;
+    - a set of resources (see {!Resource}) ;
+    - multi-sorted dependencies to other entities
+     (a set of entities, a set of functions from entity to entity,
+      a set of functions from a pair of entities to entity...).
+
+    Moreover, a timestamp corresponds to the last time the state
+    has been updated.
+
+    All the operations on the entities state are atomic (with respect
+    to Lwt), so they must be fast.
+*)
+
+open Identifier
+open Property
+
+(** {1 Definitions of entities. *)
+
+(** The entity descriptor in memory. *)
+type 'a meta deriving (Json)
+
+(** The dependencies of an entity. *)
+type dependencies
+
+(** A low-level change to the state of an entity is of
+    the following form. *)
+type 'a state_change =
+  | UpdateDependencies of dependencies
+  | UpdateResources    of Resource.name list
+  | UpdateProperties   of properties
+  | UpdateContent      of 'a
+  | UpdateSequence     of 'a state_change * 'a state_change
+  | NoUpdate
+
+val state_changes : 'a state_change list -> 'a state_change
+
+(** [identifier m] returns the identifier of [m]. *)
+val identifier : 'a meta -> identifier
+
+(** [content m] returns the content of [m]. *)
+val content : 'a meta -> 'a
+
+(** [properties m] returns the properties of [m]. *)
+val properties : 'a meta -> properties
+
+(** [sources m] returns the filenames of [m]'s sources. *)
+val sources : 'a meta -> Resource.name list
+
+(** [dependencies m] returns the dependencies of [m]. *)
+val dependencies : 'a meta -> dependencies
+
+(** [timestamp m] returns the timestamp of [m]. *)
+val timestamp : 'a meta -> float
+
+(** [now m] puts a timestamp on [m]. *)
+val now : 'a meta -> 'a meta
+
+(** Dependencies are classified using names. *)
+type dependency_kind = string
+
+(** A dependency [(y, (k, xs)] of kind [k] of an identifier [x] is a
+    function from an a list of identifiers [xs] to an identifier [y].
+    We usually write it [k (xs) = y]. *)
+type dependency = identifier * (dependency_kind * identifier list)
+
+(** No dependency. *)
+val empty_dependencies : dependencies
+
+(** [dependency_image ds] returns all the identifiers included in
+    an identifier's dependencies [ds]. *)
+val dependency_image : dependencies -> dependency list
+
+(** [dependency d k xs] returns [Some (k (xs))] if there is
+    a [y] in [d] such that [k (xs) = y]. Otherwise, returns
+    [None]. *)
+val dependency
+  : dependencies -> dependency_kind -> identifier list -> identifier option
+
+(** [all_dependencies d k] returns all the [(xs, y)] such that
+    there is a [y] in [d] such [k (xs) = y]. *)
+val all_dependencies
+  : dependencies -> dependency_kind -> (identifier list * identifier) list
+
+(** [push ds d] is the dependencies [ds] with an extra dependency [d]. *)
+val push : dependencies -> dependency -> dependencies
+
+(** [of_list ls] turns an associative list into dependencies. *)
+val of_list:
+  (dependency_kind * ((identifier list * identifier) list)) list
+  -> dependencies
+
+(** [to_list ds] transforms dependencies [ds] into an associative list. *)
+val to_list:
+  dependencies -> (dependency_kind * ((identifier list * identifier) list)) list
+
+(** {2 Public API for the implementation of the entity engine} *)
+
+(** [make id ds ps res c] returns an entity description for entity
+    [id] with dependencies [ds], properties [ps], resources [res], and
+    content [c]. *)
+val make
+  : identifier -> dependencies -> properties -> Resource.name list
+  -> 'a -> 'a meta
+
+(** [update e c] applies the low-level change [c] on the state
+    of [e]. *)
+val update : 'a meta -> 'a state_change -> 'a meta
+
+val string_of_state_change : ('a -> string) -> 'a state_change -> string
+
+val map : ('a -> 'b) -> 'a meta -> 'b meta
